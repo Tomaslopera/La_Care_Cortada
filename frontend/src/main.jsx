b74ce030be48f4562a04c70678ae3eaf0995fa23
PeterWYC1 <psa.siar@gmail.com>
2024-09-09 21:18:29 -0500

    los ultimos

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'; // Asegúrate de que este archivo existe y tiene estilos básicos
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
