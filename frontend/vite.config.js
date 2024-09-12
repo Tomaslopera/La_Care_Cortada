b5c74e622ef144a0b7148cb1c6a16aa93e99adb4
Tomaslopera <loperatomas410@gmail.com>
2024-09-12 10:18:32 -0500

    Netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // Usa el formato de módulo más nuevo
+    outDir: 'dist', // Asegúrate de que el directorio de salida sea correcto
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'no-store',
+    },
+  },
 })
